7f53854c869b9b729f9c3a06e911a44f47c6f8fd
HashWarlock <hashwarlock@protonmail.com>
2024-10-16 11:59:37 -0700

    Remove path for client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { TappdClient } from "@phala/dstack-sdk";
 
-const client = new TappdClient('../tappd.sock')
+const client = new TappdClient()
 
 export default async function hello(req, res) {
   const testQuote = await client.tdxQuote('test')
